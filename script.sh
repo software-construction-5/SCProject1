6c95dad4764e34467abb4997f141230ecb342bf0
Anthony Gufron <81062477+anthonygfrn@users.noreply.github.com>
2022-07-22 09:05:29 +0800

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,2 @@
+cd /Users/anthony/Downloads/apache-jmeter-5.5/bin
+sh jmeter.sh -Jjmeter.save.saveservice.output_format=xml -n -t /Users/anthony/Downloads/apache-jmeter-5.5/bin/JmeterJenkinsIntegrationTest.jmx -l /Users/anthony/Downloads/TestResult1.jtl
